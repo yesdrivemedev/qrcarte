d3e813c41429eac957fc07f539773914f8cb9bdb
MMFJ <baroanrufin@gmail.com>
2023-02-02 23:01:47 +0000

    Add lien dynamique OK

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # qrcarte
 Rendez votre carte facilement accesible et actualisée en temps réel. Plus besoin d'attendre un serveur.
+
+echo "# qrcarte" >> README.md
+git init
+git add README.md
+git commit -m "Landing Page OK"
+git branch -M main
+git remote add origin https://github.com/yesdrivemedev/qrcarte.git
+git push -u origin main
+
+git remote add origin https://github.com/yesdrivemedev/qrcarte.git
+git branch -M main
+git push -u origin main
+
+git pull
+git status
+git add .
+git commit -m "Landing Page OK"
+git push -u origin main
+
--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -3,6 +3,8 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Asset\Package;
+use Symfony\Component\Asset\VersionStrategy\EmptyVersionStrategy;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -21,9 +23,27 @@ class MainController extends AbstractController
         return $this->render('main/menu.html.twig');
     }
 
-    #[Route('/menu/327379006c7d21ce78bd69d59c7e5b5b/saintgermain', name: 'saintgermain')]
-    public function saintgermain()
+    #[Route('/menu/{secret}/{restau}', name: 'qrmenu')]
+    public function saintgermain($secret, $restau)
     {
-        return $this->render('main/saintgermain.html.twig');
+
+        $filepath  = $this->getParameter('kernel.project_dir') . '\public\json\restautants_db.json';
+        $json_string = file_get_contents($filepath);
+        $json = json_decode($json_string, true);
+        //Steven Miller - Red
+
+        $nom = 'Zeze';
+
+        foreach ($json as $elem) {
+            //  echo ($elem['name'] . " - " . $elem['favourite']['statut']);
+            if ($elem['code'] == $restau && $elem['token']['statut'] == 'OK' && $elem['token']['secret'] == $secret)  $nom = $elem['name'];
+            //echo ("<br/>");
+        }
+
+
+        // echo ("<br/>");
+        // echo $nom;
+        //  die();
+        return $this->render('main/qrmenu.html.twig', ['nom' => $nom]);
     }
 }
